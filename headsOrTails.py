a87e029cfee58df80c32915b576be29e8a135841
koki <youyehongji@gmail.com>
2024-07-02 11:02:03 +0900

    Victory message added

--- headsOrTails.py
+++ headsOrTails.py
@@ -10,3 +10,8 @@ for i in range(3):
 heads_count = results.count("Heads")
 tails_count = results.count("Tails")
 print(f"Heads: {heads_count}, Tails: {tails_count}")
+
+if heads_count > tails_count:
+    print("You won!")
+else:
+    print("You lost!")
